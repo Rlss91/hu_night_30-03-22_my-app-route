1ef327fa756457a7b4aacf16a58835f1e7889634
rlss91 <rlss91@gmail.com>
2022-03-30 19:41:25 +0300

    first setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { ToastContainer } from "react-toastify";
+import NavbarComponent from "./components/NavbarComponent";
+
+function App() {
+  return (
+    <div className="container">
+      <NavbarComponent />
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+// add bootstrap to react
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+//react-toastify
+import "react-toastify/dist/ReactToastify.css";
+
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+//axios config
+axios.defaults.baseURL = "http://localhost:3003/api";
+
+axios.interceptors.request.use((config) => {
+  config.headers["x-user-token"] =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjQwOThmMzczZTE4ODgyM2FlY2Q5NDEiLCJiaXoiOnRydWUsImlhdCI6MTY0ODQwMjIyNH0.EepyQ0AJZ-qlA3qs55kdJc8ia5JAYeFKdp27r_FHNa0";
+  config.headers["Content-Type"] = "application/json";
+  return config;
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
